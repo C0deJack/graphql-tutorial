3143d145b937585f94e508348682b98b215dc545
Jack <jackwoodpearce@gmail.com>
2021-12-23 15:50:40 +0000

    Create db models and Connect to online db to return data

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://C0deJack:tutorial_gql98675322567@cluster0.2qm8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+mongoose.connection.once('open', () => {
+	console.log('connected to DB');
+
+
+})
+
 app.use('/graphql', graphqlHTTP({
-	schema,
+	schema, // GraphQL Schema. Not to be confused with the db schemas found in models folder.
 	graphiql: true
 }));
 
